e4eef7e65f21143801784cc1967ebc9a708c5709
Julio César Pastor Marcelo <pazthor@ilovecheesypoofs.com>
2018-07-29 15:00:33 -0500

    Add files for solving problems in Scala

---
+++ ScalaProblems/src/Alfa/Semana1/Problema1.scala
@@ -0,0 +1,6 @@
+package Alfa.Semana1
+
+class Problema1 {
+  def main(args: Array[String]){}
+
+}
--- ScalaProblems/src/Alfa/Semana1/TheatreSquare.scala
+++ ScalaProblems/src/Alfa/Semana1/TheatreSquare.scala
@@ -1,8 +1,9 @@
-import scala.io.Source
+package Alfa.Semana1
 
-object test{
+object TheatreSquare{
   def main(args: Array[String]){
     val s = readLine().split(" ").map(_.toInt)
+
     val fn = (x: Long, y: Long) => (x + y - 1)/y
     val n = fn(s(0), s(2))
     val m = fn(s(1), s(2))
