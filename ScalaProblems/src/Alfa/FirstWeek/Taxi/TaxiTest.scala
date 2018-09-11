667f7df03577b2c4ac8f6439f15723fb1ac8149a
Julio César Pastor Marcelo <pazthor@ilovecheesypoofs.com>
2018-09-11 14:00:48 -0500

    Update object Taxi: Add method main for the solution Taxi.

--- ScalaProblems/src/Alfa/FirstWeek/Taxi/TaxiTest.scala
+++ ScalaProblems/src/Alfa/FirstWeek/Taxi/TaxiTest.scala
@@ -1,11 +1,15 @@
 package Alfa.FirstWeek.Taxi
 
+
+import scala.io.StdIn
 object TaxiTest {
   def main(args: Array[String]){
-    val s = readLine().split(" ").map(_.toInt)
+    val numberOfGroups = StdIn.readInt()
+    val childrens = StdIn.readLine().split(" ").map(_.toInt)
 
-    val fn = (x: Int) => if(x % 2 == 0 && x > 2  ) "YES" else "NO"
-    val n = fn(s(0))
-    println(n)
+    val taxi = new Taxi()
+    val res = taxi.process(childrens)
+
+    println(res)
   }
 }
