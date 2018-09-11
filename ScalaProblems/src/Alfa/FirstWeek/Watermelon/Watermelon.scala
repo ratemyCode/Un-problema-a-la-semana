2b99c01f9e4d51c3ff3b0f79b6bba98fae9a0f71
Julio César Pastor Marcelo <pazthor@ilovecheesypoofs.com>
2018-09-11 14:01:20 -0500

    Update WaterMelon: Add solution for the problem WaterMelon

--- ScalaProblems/src/Alfa/FirstWeek/Watermelon/Watermelon.scala
+++ ScalaProblems/src/Alfa/FirstWeek/Watermelon/Watermelon.scala
@@ -1,5 +1,9 @@
-package Alfa.FirstWeek.TheatreSquare.FirstWeek.Watermelon
+package Alfa.FirstWeek.Watermelon
 
 class Watermelon {
-
+  def process(weightWaterMelon: Int): String ={
+    val fn = (x: Int) => if(x % 2 == 0 && x > 2  ) "YES" else "NO"
+    val n = fn(weightWaterMelon)
+    n
+  }
 }
--- ScalaProblems/src/Alfa/FirstWeek/Watermelon/WatermelonTest.scala
+++ ScalaProblems/src/Alfa/FirstWeek/Watermelon/WatermelonTest.scala
@@ -1,5 +1,14 @@
 package Alfa.FirstWeek.Watermelon
 
-object WatermelonTest {
+import scala.io.StdIn
 
+object WatermelonTest {
+  def main(args: Array[String]): Unit ={
+//    val s = readLine().split(" ").map(_.toInt)
+    val s = StdIn.readInt()
+    val waterMelon = new Watermelon
+    val res  = waterMelon.process(s)
+
+    print(res)
+  }
 }
