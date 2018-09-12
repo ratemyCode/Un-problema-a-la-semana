e0b6acd0b4e0e99a718ba4e4043d6d6f04f09ec4
Julio César Pastor Marcelo <pazthor@ilovecheesypoofs.com>
2018-09-12 17:50:56 -0500

    Add solution for Soldier and Bananas;

--- ScalaProblems/src/Alfa/ThirdWeek/SoldierAndBananas/SoldierAndBananas.scala
+++ ScalaProblems/src/Alfa/ThirdWeek/SoldierAndBananas/SoldierAndBananas.scala
@@ -1,5 +1,14 @@
 package Alfa.ThirdWeek.SoldierAndBananas
 
 class SoldierAndBananas {
+  def process(k: Int, n: Int, w: Int): Int ={
+    var sum = 0
 
+    for( i <- 1 to w){
+      sum = sum + i * k
+    }
+    if ( n - sum   >= 0 ) return 0
+
+    sum - n
+  }
 }
--- ScalaProblems/src/Alfa/ThirdWeek/SoldierAndBananas/SoldierAndBananasMain.scala
+++ ScalaProblems/src/Alfa/ThirdWeek/SoldierAndBananas/SoldierAndBananasMain.scala
@@ -1,5 +1,17 @@
 package Alfa.ThirdWeek.SoldierAndBananas
 
-object SoldierAndBananasMain {
+import scala.io.StdIn
 
+object SoldierAndBananasMain {
+  def main(args: Array[String]): Unit = {
+    val knw = StdIn.readLine().split(" ").map(_.toInt)
+    val k = knw(0)
+    val n = knw(1)
+    val w = knw(2)
+
+    val saB = new SoldierAndBananas
+    val res = saB.process(k, n, w)
+    print(res)
+
+  }
 }
