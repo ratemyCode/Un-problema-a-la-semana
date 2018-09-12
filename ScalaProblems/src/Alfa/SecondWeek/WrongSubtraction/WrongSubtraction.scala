0e08a9fb0cd6f875d3a7100a258f6bc55c8cb084
Julio César Pastor Marcelo <pazthor@ilovecheesypoofs.com>
2018-09-12 17:05:24 -0500

    Add solution for WrongSubtraction.

--- ScalaProblems/src/Alfa/SecondWeek/WrongSubtraction/WrongSubtraction.scala
+++ ScalaProblems/src/Alfa/SecondWeek/WrongSubtraction/WrongSubtraction.scala
@@ -2,4 +2,11 @@ package Alfa.SecondWeek.WrongSubtraction
 
 class WrongSubtraction {
 
+
+  def process (n: Int, k: Int): Int ={
+    if( k == 0) return n
+    if(n % 10 == 0)  return process(n/10, k -1)
+    else return process(n -1, k -1)
+  }
+
 }
--- ScalaProblems/src/Alfa/SecondWeek/WrongSubtraction/WrongSubtractionTest.scala
+++ ScalaProblems/src/Alfa/SecondWeek/WrongSubtraction/WrongSubtractionTest.scala
@@ -1,5 +1,16 @@
 package Alfa.SecondWeek.WrongSubtraction
 
+import scala.io.StdIn
+
 object WrongSubtractionTest {
 
+  def main(args:Array[String]): Unit={
+    val nk = StdIn.readLine().split(" ").map(_.toInt)
+    val n = nk(0)
+    val k = nk(1)
+    val wrongSubtraction = new WrongSubtraction
+    val res = wrongSubtraction.process(n,k)
+
+    print(res)
+  }
 }
