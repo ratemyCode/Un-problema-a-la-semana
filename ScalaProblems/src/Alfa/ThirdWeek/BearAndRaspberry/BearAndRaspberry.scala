1e47a3c4901240ee3197acfc4b60e67562e55a19
Julio César Pastor Marcelo <pazthor@ilovecheesypoofs.com>
2018-09-12 17:35:04 -0500

    Add solution for the problem Bear and Raspberry

--- ScalaProblems/src/Alfa/ThirdWeek/BearAndRaspberry/BearAndRaspberry.scala
+++ ScalaProblems/src/Alfa/ThirdWeek/BearAndRaspberry/BearAndRaspberry.scala
@@ -1,5 +1,17 @@
-package Alfa.ThirdWeek
+package Alfa.ThirdWeek.BearAndRaspberry
 
 class BearAndRaspberry {
+
+  def process(n: Int, c: Int, elements:Array[Int]): Int ={
+    var LOL = 0
+
+    for(i <- 0 to n-2){
+      val d  =  elements(i) - c - elements(i+1)
+      if( LOL < d)
+        LOL = d
+    }
+
+    LOL
+  }
 
 }
