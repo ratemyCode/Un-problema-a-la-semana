64060134d8cabb62f18a3ecbd0f6089262e41fb7
Julio César Pastor Marcelo <pazthor@ilovecheesypoofs.com>
2018-09-30 21:18:59 -0500

    Update files: Add solutions for 2 problems.

--- ScalaProblems/src/Beta/Week_1/CoverPoints/CoverPoints.scala
+++ ScalaProblems/src/Beta/Week_1/CoverPoints/CoverPoints.scala
@@ -1,5 +1,18 @@
 package Beta.Week_1.CoverPoints
 
-class CoverPoints {
+import scala.io.StdIn
+
+object CoverPoints {
+  def main(args: Array[String]): Unit ={
+    val n = StdIn.readInt()
+    var ans = 0
+    for( i <- 1 to n){
+      val xy = StdIn.readLine().split(" ").map(_.toInt)
+      val x = xy(0)
+      val y = xy(1)
+      ans  = math.max(ans,x + y)
+    }
+    print(ans)
+  }
 
 }
--- ScalaProblems/src/Beta/Week_1/LittleCLoves3_I/LittleCLoves3Main.scala
+++ ScalaProblems/src/Beta/Week_1/LittleCLoves3_I/LittleCLoves3Main.scala
@@ -1,5 +1,41 @@
 package Beta.Week_1.LittleCLoves3_I
 
-object LittleCLoves3Main {
+import scala.io.StdIn
 
+object LittleCLoves3Main {
+  def main(args: Array[String]){
+    val n = StdIn.readInt()
+    val p = n/3
+    val tt = p%3
+    val resFaltante = n%3
+    print("residuo de tt:" + tt)
+
+
+    print(matches(resFaltante, p,tt))
+  }
+
+  def matches(x:Int, p: Int, tt: Int): String = {
+    if(x == 1) {
+      if(tt == 0){
+        return ""+(p -1) + " " + (p + 1) + " " + (p + 1)
+      }
+
+      if( (p + 1) % 3 != 0  )
+        return p + " " + p  + " " + (p + 1)
+      else return p + " " + (p + 2) + " " + (p - 1)
+    }
+    if(x == 2) {
+      if(tt == 0){
+        return (p -1) + " " + (p + 2) + " " + (p + 1)
+      }
+      if((p + 2) % 3 != 0)
+        return p + " " + p  + " " + (p + 2)
+      else return p + " " + (p + 1)  + " " + (p + 1)
+    }
+    if(x == 0 && tt == 0){
+      return (p+1) + " " + (p-2) + " " + (p+1)
+    }
+    return p + " " + p  + " " + p
+
+  }
 }
